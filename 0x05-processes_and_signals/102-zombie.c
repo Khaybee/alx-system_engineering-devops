0ce1f5126c927335baa7601c2cd1c4f87dbcee95
Khaybee <kachieosuji11@gmail.com>
2023-07-28 17:26:08 -0700

    zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+		else if (child_pid == 0)
+		{
+			/* This is the child process; immediately exit to become a zombie*/
+			exit(0);
+		}
+		else
+		{
+			perror("fork");
+			exit(1);
+		}
+	}
+
+	infinite_while();
+	return (0);
+}
